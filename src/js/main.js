8b5e39ab297fd3c392f3d015fdf0eb91d942e870
Andrej Spinej <enjoyhillol@gmail.com>
2024-02-10 07:50:23 +1000

    add new fn

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,55 @@
 let typed = new Typed('#element', {
-    strings: ['Создаю web интерфейсы.', 'Занимаюсь разработкой и доработкой сайтов.'],
+    strings: [
+        'Создаю web интерфейсы.',
+        'Занимаюсь разработкой и доработкой сайтов.',
+    ],
     typeSpeed: 80,
     loop: true,
 });
+
+// Highlight mouse hover
+
+let originalBGplaypen = $('#playpen').css('background-color'),
+    x,
+    y,
+    xy,
+    bgWebKit,
+    bgMoz,
+    lightColor = 'rgba(101,255,127,0.077)',
+    gradientSize = 377;
+
+$('#playpen')
+    .mousemove(function (e) {
+        x = e.pageX - this.offsetLeft;
+        y = e.pageY - this.offsetTop;
+        xy = x + ' ' + y;
+
+        bgWebKit =
+            '-webkit-gradient(radial, ' +
+            xy +
+            ', 0, ' +
+            xy +
+            ', ' +
+            gradientSize +
+            ', from(' +
+            lightColor +
+            '), to(rgba(255,255,255,0.0))), ' +
+            originalBGplaypen;
+        bgMoz =
+            '-moz-radial-gradient(' +
+            x +
+            'px ' +
+            y +
+            'px 45deg, circle, ' +
+            lightColor +
+            ' 0%, ' +
+            originalBGplaypen +
+            ' ' +
+            gradientSize +
+            'px)';
+
+        $(this).css({ background: bgWebKit }).css({ background: bgMoz });
+    })
+    .mouseleave(function () {
+        $(this).css({ background: originalBGplaypen });
+    });
