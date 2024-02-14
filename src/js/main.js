cfef0e2d00b31326e215e389a9829bd317d3fba6
Andrej Spinej <enjoyhillol@gmail.com>
2024-02-14 20:22:56 +1000

    Final release in prod

--- src/js/main.js
+++ src/js/main.js
@@ -77,3 +77,40 @@ function fadeIn() {
         footerContacts.classList.remove('fade-in');
     }
 }
+
+
+// Анимация элементов при scroll
+
+// Функция для выезда с лево
+function handleScrollAnimationLeft() {
+    let elementsLeft = document.querySelectorAll('.scroll-animation-left');
+    let windowHeight = window.innerHeight;
+
+    elementsLeft.forEach(function(element) {
+        let position = element.getBoundingClientRect().top;
+
+        if (position < windowHeight) {
+        element.classList.add('fade-in');
+    }
+    });
+}
+
+// Функция для выезда с право
+function handleScrollAnimationRight() {
+    let elementsRight = document.querySelectorAll('.scroll-animation-right');
+    let windowHeight = window.innerHeight;
+
+    elementsRight.forEach(function(element) {
+        let position = element.getBoundingClientRect().top;
+
+        if (position < windowHeight) {
+        element.classList.add('fade-in');
+    }
+    });
+}
+// left
+window.addEventListener('scroll', handleScrollAnimationLeft);
+window.addEventListener('load', handleScrollAnimationLeft);
+// right
+window.addEventListener('scroll', handleScrollAnimationRight);
+window.addEventListener('load', handleScrollAnimationRight);
